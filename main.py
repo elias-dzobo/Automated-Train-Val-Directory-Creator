c5ae00ab08fe18462f1415287b765403d12fe94d
elias-dzobo <elias.dzobo@gmail.com>
2022-06-23 21:38:46 +0000

    automation phase

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
 import os 
+import imghdr 
 
-root = './'
+root = '../Cocoa Price Prediction/'
 
-for subdirs, dirs, files in os.walk(root):
-    print(len(os.listdir(subdirs))) 
+def getSubDirCount(root):
+    subDir = []
+    for subdirs in next(os.walk(root))[1]:
+        subDir.append(subdirs)
+    return len(subDir)
+
+def fileCount(directory):
+    path = os.path.join(root, directory)
+    count = len(os.listdir(path))
+    return count 
+
+def createTrainingDirectory():
+    path = 'Training'
+    completepath = os.path.join(root, path)
+    os.mkdir(completepath)
+    return completepath
+
+def createValidationDirectory():
+    path = 'Validation'
+    completepath = os.path.join(root, completepath)
+    os.mkdir(completepath)
+    return completepath
+
+def isImageFile(file):
+    fileType = imghdr.what(file)
+    if fileType:
+        return True 
+    else:
+        return False
+
+def countSubDirectories(root):
+    pass 
+
+if __name__ == '__main__':
+    print(isImageFile('test.py'))
